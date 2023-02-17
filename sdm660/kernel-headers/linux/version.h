6c23e8e2eff19aef3d02e654a826c5095e120355
Mocarafee <harif.rp05@gmail.com>
2023-02-18 00:45:11 +0700

    prebuilt: switch over to minimal kernel

--- sdm660/kernel-headers/linux/version.h
+++ sdm660/kernel-headers/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 67371310
-#define KERNEL_VERSION(a,b,c) (((a) << 24) + ((b) << 16) + (c))
+#define LINUX_VERSION_CODE 263423
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
